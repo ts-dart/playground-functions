71f328bbb16efc0ba592c8e0b6277f1fe6f8d0fb
Thiago Henrique <thiagoedusan5.11@outlook.com>
2022-08-06 17:11:30 -0300

    Buscando 100% no pr

--- src/challenges2.js
+++ src/challenges2.js
@@ -33,8 +33,6 @@ function generatePhoneNumber(arr) {
   return `(${arr[0]}${arr[1]}) ${arr[2]}${arr[3]}${arr[4]}${arr[5]}${arr[6]}-${arr[7]}${arr[8]}${arr[9]}${arr[10]}`
 }
 
-console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 0, 0, 0]));
-
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
   // seu código aqui
