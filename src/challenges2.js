2efbdac2107e7802b4d1d5fa1e67c4e988513208
Thiago Henrique <thiagoedusan5.11@outlook.com>
2022-08-08 16:15:42 -0300

    testando avaliador

--- src/challenges2.js
+++ src/challenges2.js
@@ -29,8 +29,6 @@ function generatePhoneNumber(arr) {
     if (numbers.length >= 3) return 'não é possível gerar um número de telefone com esses valores';
   };
   
-  console.log('testando');
-  
   return `(${arr[0]}${arr[1]}) ${arr[2]}${arr[3]}${arr[4]}${arr[5]}${arr[6]}-${arr[7]}${arr[8]}${arr[9]}${arr[10]}`
 }
 
